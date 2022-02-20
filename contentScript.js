02dea0b30e4e9af87b8f4d5769dc0dced613b4ce
胡浩 <huhao@192.168.1.6>
2022-02-21 01:30:40 +0800

    feat: update

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,22 @@
 function afterWindowLoaded(){
   setTimeout(()=>{
+    
     var btn = document.querySelector('.ytp-chapter-title-prefix')    
     btn?.click()  
-   },3000)  // some extension (ad-blockers...) would affect LCP
+
+    const btm1 = document.querySelector('.ytp-chrome-bottom')
+    const btm2 = document.querySelector('.ytp-chrome-controls')
+    const btm3 = document.querySelector('.ytp-progress-bar-container')
+    btm1.style.opacity='1'
+    btm2.style.opacity='1'
+    btm3.style.opacity='1'
+    let video = document.querySelector("#movie_player")
+
+    video?.dispatchEvent(new Event('mousemove'));
+    setInterval(function(){
+      video?.dispatchEvent(new Event('mousemove'));
+    },2000);
+   },3000)
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
