7eb4dcc0df8c705083b94b3e5aa660cde89ec39f
eret9616 <947416983@qq.com>
2022-04-25 02:45:56 +0800

    chore: usename

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(changeInfo?.status == "complete"){
         if(!tab.url.includes('https://www.youtube.com/watch')){
             return
-        }
+        } 
         chrome.tabs.sendMessage(tabId, {data: tab});
     }
 });
